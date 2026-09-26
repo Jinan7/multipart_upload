409dac1c071cf0321b9f47fbf2c4e6c2629b7937
Jinan <lakanj25@gmail.com>
2026-09-26 15:27:02 +0100

    sep 26 15:27

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{fs::File, io::Write, path::Path};
-
+use std::{fs::File, io::Write, path::Path, sync::{Arc, Mutex}, thread::JoinHandle};
 use anyhow::Context;
 use aws_config::{BehaviorVersion, SdkConfig, retry::RetryConfig};
-use aws_sdk_s3::{operation::{complete_multipart_upload::{CompleteMultipartUploadOutput}, create_multipart_upload::CreateMultipartUploadOutput, upload_part::UploadPartOutput}, primitives::{ByteStream, Length}, types::{CompletedMultipartUpload, CompletedPart}};
+use aws_sdk_s3::{operation::{complete_multipart_upload::CompleteMultipartUploadOutput, create_multipart_upload::CreateMultipartUploadOutput, upload_part::UploadPartOutput}, primitives::{ByteStream, Length, event_stream::HeaderValue::Uuid}, types::{CompletedMultipartUpload, CompletedPart}};
 use rand::{RngExt, distr::Alphanumeric};
+use watchexec::{Watchexec, error::CriticalError};
+use watchexec_events::{Event, Tag::FileEventKind, filekind::FileEventKind::{Modify, Remove}};
+use watchexec_signals::Signal;
 const CHUNK_SIZE: u64 = 1024 * 1024 * 5;
 const MAX_CHUNKS: u64 = 10_000;
 #[tokio::main]
@@ -16,17 +18,17 @@ async fn main() -> Result<(), anyhow::Error>{
 async fn start_multipart_upload() -> Result<(), anyhow::Error> {
 
     let bucket_name = "ub-s3-j1-iam";
-    let key = "new_text_file.txt";
+    let key = format!("new_text_file-{}.txt", uuid::Uuid::new_v4());
     
     let config = get_config().await?;
     
     let client = aws_sdk_s3::Client::new(&config);
     
-    let multipartupload_res = create_multipart_upload(&client, bucket_name, key).await?;
+    let multipartupload_res = create_multipart_upload(&client, bucket_name, &key).await?;
     let upload_id = multipartupload_res.upload_id().ok_or(anyhow::anyhow!("No upload ID"))?;
     println!("{upload_id}");
-    let _file = create_file(key)?;
-    upload_parts(key, &client, bucket_name, key, upload_id).await?;
+    let _file = create_file(&key)?;
+    upload_parts(&key, &client, bucket_name, &key, upload_id).await?;
 
 
     Ok(())
@@ -50,7 +52,12 @@ async fn upload_parts(
     key: &str,
     upload_id: &str,
 ) -> Result<(), anyhow::Error> {
+    //get path and set file watcher on path
     let path = Path::new(file_path);
+    //atomic boolean to track if file has been modified
+    let modified = Arc::new(Mutex::new(false));
+    let wx = get_file_watcher(path, Arc::clone(&modified)).await?;
+
     let file_size = tokio::fs::metadata(path)
         .await
         .expect("file not found")
@@ -74,6 +81,8 @@ async fn upload_parts(
 
     let mut upload_parts: Vec<aws_sdk_s3::types::CompletedPart> = Vec::new();
 
+    //start file watcher
+    let handle = wx.main();
     for chunk_index in 0..chunk_count {
         let chunk_size = if chunk_count - 1 == chunk_index {
             size_of_last_chunk
@@ -86,8 +95,7 @@ async fn upload_parts(
             .offset(chunk_index * CHUNK_SIZE)
             .length(Length::Exact(chunk_size))
             .build()
-            .await
-            .unwrap();
+            .await?;
 
         let part_number = (chunk_index as i32) + 1;
         let upload_part_res = upload_part(client, bucket_name, key, upload_id, part_number, stream).await?;
@@ -98,6 +106,15 @@ async fn upload_parts(
                 .part_number(part_number)
                 .build()
         );
+
+        //abort upload if file was modified during upload
+        if *modified.lock().unwrap() == true {
+            //abort upload
+            abort_multipart_upload(client, bucket_name, upload_id, key).await?;          
+            close_file_watcher(wx, handle).await?;           
+            //early return an error
+            return Err(anyhow::anyhow!("Error reading file"));
+        }
     }
 
     let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
@@ -105,10 +122,22 @@ async fn upload_parts(
         .build();
 
     complete_multipart_upload(client, bucket_name, key, upload_id, completed_multipart_upload).await?;
-
+    close_file_watcher(wx, handle).await?;
+    
     Ok(())
 }
 
+async fn close_file_watcher(
+    watcher: Arc<Watchexec>,
+    handle: tokio::task::JoinHandle<Result<(), CriticalError>>
+) -> Result<(), anyhow::Error>{
+
+    //send terminate event to file watcher
+    watcher.send_event(Event::default(), watchexec_events::Priority::Urgent).await?;
+    //block on file watcher to finish thread
+    handle.await?;
+    Ok(())
+}
 async fn upload_part(
     client: &aws_sdk_s3::Client,
     bucket_name: &str,
@@ -150,6 +179,25 @@ async fn create_multipart_upload(
     Ok(multipart_upload_res)
 }
 
+async fn abort_multipart_upload(
+    client: &aws_sdk_s3::Client,
+    bucket: &str,
+    upload_id: &str,
+    key: &str
+) -> Result<(), anyhow::Error> {
+
+    let res = client
+        .abort_multipart_upload()
+        .bucket(bucket)
+        .upload_id(upload_id)
+        .key(key)
+        .send()
+        .await
+        .context("Error aborting multipart upload")?;
+
+    Ok(())
+}
+
 async fn complete_multipart_upload(
     client: &aws_sdk_s3::Client,
     bucket: &str,
@@ -188,6 +236,41 @@ fn create_file(
     }
 
     Ok(file)
+}
+
+async fn get_file_watcher(
+    file_path: &Path,
+    modified: Arc<Mutex<bool>>
+) -> Result<Arc<Watchexec>, anyhow::Error> {
+
+    let wx = Watchexec::new(move |mut action| {
+
+        for event in action.events.iter() {
+
+            for tag in event.tags.iter() {
+
+                if let FileEventKind(event_kind) = tag {
+
+                    match event_kind {
+                        Modify(_) => *modified.lock().unwrap() = true,
+                        Remove(_) => *modified.lock().unwrap() = true,
+                        _ => ()
+                    }
+                }
+            }
+        }
+
+        if action.signals().any(|sig|{ matches!(sig, Signal::Interrupt | Signal::Terminate)}) {
+            action.quit()
+        }
+
+        action
+
+    })?;
+
+    wx.config.pathset([file_path]);
+
+    Ok(wx)
 }
 
 
